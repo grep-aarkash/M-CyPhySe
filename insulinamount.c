93211a24a7af4f6316f171736e7871cb5647d215
grep-aarkash <kaarti.sr@gmail.com>
2019-04-07 14:19:15 -0700

    Updated prototype-1

---
+++ insulinamount.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/* The v1 of the prototype. It takes into consideration the basal_rates, insulin levels and the blood
+glucose defined by the user. Every time the program is run, it generates a random insulin inside the body
+value and the blood glucose of the person. The basal_rate which is the rate of metabolism change in a user
+is kept fixed by me for the time being.
+*/
+int int main(int argc, char const *argv[]) {
+
+  // Define the three terms which are being used in the
+  int basal_rates = 2;
+  int insulin =  rand() % 50;
+  int blood_glucose = rand() % 50;
+
+  //In order to maintain stability, this is the value of the target_bg
+  int target_bg = 45;
+  //
+  int
+/*The delta difference, is the parameter which tells if insulin needs to be injected
+*/
+  int delta = blood_glucose - target_bg;
+
+  return 0;
+}
